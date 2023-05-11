41eccdcf1391e37349e6cb7c4b4b66bc2b4f34b2
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-05-11 23:37:13 +0530

    Add files via upload
    This is a simple addition program ..

---
+++ Add3Num.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Add3Num {
+	public static void main(String[] args) {
+		short a = 10;
+		short b = 20;
+		short c = 20;
+		int sum = a + b + c;
+		System.out.println("The Sum Of Three Num is" + sum);
+	}
+}
