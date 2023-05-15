e91d90198fb48b33b3f0c74aaf7b313137a26cba
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-05-15 12:33:12 +0530

    Add files via upload
    java operators are symbol that are used to perform operations on variable and manipulate tha values of the operands .
     Each operator performs specific operations...

---
+++ CWH_07_CH2operator.java
@@ -0,0 +1,26 @@
+package basic;
+
+public class CWH_07_CH2operator {
+	public static void main(String[] args) {
+int a=3;
+	int b=8*a;
+	int d=8%a;  //modulo operator
+	System.out.println(d);
+
+//		int b = 9;
+//		b *= 3;
+//		System.out.println(b);
+
+		// comparision operator
+
+//	System.out.println(65<6); 
+//	System.out.println(6==5);
+//	System.out.println(5==5);
+
+		// logical operator
+//		System.out.println(64 < 5 && 65 < 6);
+//		System.out.println(40 > 5 && 66 > 3);
+//		System.out.println(55 > 6 || 66 > 99);
+
+	}
+}
