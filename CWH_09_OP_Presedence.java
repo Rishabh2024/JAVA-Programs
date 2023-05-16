d2598b7715b04fdddc86a34e98efb8799e49b94f
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-05-16 16:26:31 +0530

    Add files via upload
    The operator's precedence refers to the order in which operators are evaluated within an expression whereas associativity refers to the order in which the consecutive operators within the same group are carried out. Precedence rules specify the priority (which operators will be evaluated first) of operators.

---
+++ CWH_09_OP_Presedence.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class CWH_09_OP_Presedence {
+	public static void main(String[] args) {
+//		int a = 6 * 5 - 34 / 2;
+//	int b=60/5-34*2;
+//		System.out.println(a);
+//	System.out.println(b);
+
+//	precedence & Associativity
+    
+//	quiz
+	int b=1;
+	int c=4;
+	int a=5;
+	int k=b*b-(4*a*c)/(2*a);
+	System.out.println(k);
+	}
+}
