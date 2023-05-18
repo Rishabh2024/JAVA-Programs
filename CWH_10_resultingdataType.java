83f7bc81281be90534dca9fe69085f47e7d90cf3
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-05-18 12:05:58 +0530

    Add files via upload
    Resulting Data type ..with Increment Decrement Data type...

---
+++ CWH_10_resultingdataType.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class CWH_10_resultingdataType {
+public static void main(String[] args) {
+	
+	byte x=5;
+//	int y=6;
+	short z=8;
+//	int a=y+z;
+	float b=6.54f+x;
+	System.out.println(b);
+	
+//	Increment &Decrement Operator
+	
+//	int i=55;
+//	System.out.println(i++);
+//	System.out.println(++i);
+	
+	int y=7;
+	System.out.println(++y*8);
+}
+}
