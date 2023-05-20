6f221061e85b9d6ba2be7090644de442a32e4c82
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-05-20 13:49:32 +0530

    Add files via upload
    This is an String Based program Where user can give input and Simply it will be print on console ..

---
+++ CWH_13_String.java
@@ -0,0 +1,24 @@
+package core;
+
+import java.util.Scanner;
+
+public class CWH_13_String {
+public static void main(String[] args) {
+	String name=new String("Rishabh");
+	String name1="Rishabh ";
+	System.out.printf(name1);
+	
+//	int a=6;
+//	float b=6.5529f;
+	
+//	System.out.printf("The value of a is %d and value of b is %f  ",a,b);
+	
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("write here");
+	String st=sc.next();
+	String stt=sc.nextLine();
+	System.out.println(stt);
+	
+}
+}
