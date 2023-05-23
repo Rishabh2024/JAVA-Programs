b304443abffc5cfa2ca1a6d30defcbf6f47c70aa
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-05-23 13:41:01 +0530

    Add files via upload
    This is a string method in java programming language ....
    Java String class provides a lot of methods to perform operations on strings such as compare(), concat(), equals(), split(), length(), replace(), compareTo(), intern(), substring() etc.

---
+++ CWH_14_StringMethods.java
@@ -0,0 +1,40 @@
+package core;
+
+public class CWH_14_StringMethods {
+public static void main(String[] args) {
+	String name="Rishabh";
+//	System.out.println(name);
+	System.out.println(name.length());
+//	int value=name.length();
+//	System.out.println(value);
+//	System.out.println(name.toLowerCase());
+//	System.out.println(name.toUpperCase());
+	
+	
+//	String nonTrimmedString="       Rishabh   ";
+//	System.out.println(nonTrimmedString);
+//	String trimmedString=nonTrimmedString.trim();
+//	System.out.println(trimmedString);
+
+//	System.out.println(name.substring(3));
+//	System.out.println(name.substring(2, 5));
+	
+//	System.out.println(name.replace('s', 'p'));
+	
+//	System.out.println(name.startsWith("Ri"));
+//	System.out.println(name.endsWith("abh"));
+	
+	
+//	System.out.println(name.charAt(6));
+	
+//	System.out.println(name.indexOf("h"));
+//	System.out.println(name.indexOf("h", 5));
+	
+//	System.out.println(name.lastIndexOf("b"));
+//	System.out.println(name.lastIndexOf("ab", 6));
+	
+//	System.out.println(name.equals("Rishabh"));
+//	System.out.println(name.equalsIgnoreCase("Rishabh"));
+	
+}
+}
