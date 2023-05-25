dd1865cdb8b3b212d016881b692ae356f629675f
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-05-25 14:29:55 +0530

    Add files via upload
    Here I  practice  Some string method question ...

---
+++ CWH_15_ps3.java
@@ -0,0 +1,31 @@
+package core;
+
+public class CWH_15_ps3 {
+public static void main(String[] args) {
+	
+//	problem1	
+//	 String name= "RishaBH PAL";
+//	 System.out.println(name.toLowerCase());
+	 
+//	 problem2
+//	 String text="to lower        Case";
+//	 text=text.replace("replacement", "_");
+//	 System.out.println(text);
+	     
+
+//	 problem 3
+//	 String letter="Dear <|name|> Thanks a lot";
+//	letter= letter.replace("<|name|>", "rishabh");
+//	System.out.println(letter);
+	
+//	problem 4
+//	String mystring ="hi rishabh     this        side";
+//	System.out.println(mystring.indexOf("    "));
+//	System.out.println(mystring.indexOf(" "));
+	
+//	problem 5
+	
+	String letter2="rishabh \n akash \n shubham \n sahil \n ankit \n deepak";
+	System.out.println(letter2);
+}
+}
