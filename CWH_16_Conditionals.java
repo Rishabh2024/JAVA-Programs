ce9704d25026e6dfac1f5947d4831f49f6677c6f
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-06-27 12:23:20 +0530

    Add files via upload
    Conditional statements in Java are used to make decisions based on certain conditions. The most common conditional statements in Java are the If-Else statement .

---
+++ CWH_16_Conditionals.java
@@ -0,0 +1,12 @@
+package core;
+
+public class CWH_16_Conditionals {
+	public static void main(String[] args) {
+		int age = 15;
+		if (age > 18) {
+			System.out.println("boy you can drive");
+		} else {
+			System.out.println("boy you can't  drive");
+		}
+	}
+}
