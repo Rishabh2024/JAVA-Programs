48323ae503c137d51dc62490cfc2a4750624707e
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-06-28 10:32:03 +0530

    Add files via upload
    logical conditional in java (boolean).

---
+++ CWH_17_Logical.java
@@ -0,0 +1,23 @@
+package core;
+
+public class CWH_17_Logical {
+	public static void main(String[] args) {
+	boolean a= true;
+	boolean b= false;  
+	if(a && b) {
+		System.out.println("Y");
+	}
+	else {
+		System.out.println("N");
+	}
+
+//		System.out.println("for logical OR");
+//		boolean a1 = true;
+//		boolean b2 = false;
+//		if (a1 || b2) {
+//			System.out.println("Y");
+//		} else {
+//			System.out.println("N");
+//		}
+	}
+}
