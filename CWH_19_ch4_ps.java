d46f4382965958fdfd6ea63a70047339389e7299
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-03 11:37:04 +0530

    Add files via upload
    exercise of finding student marks and weekends...

---
+++ CWH_19_ch4_ps.java
@@ -0,0 +1,55 @@
+package core;
+
+import java.util.Scanner;
+
+public class CWH_19_ch4_ps {
+public static void main(String[] args) {
+//	Question 1
+//	int a=11;
+//	if(a==11) {
+//		System.out.println("I am Go");
+//	}
+//	else {
+//		System.out.println("No I am not go");
+//	}
+	
+//	question2
+//	byte m1,m2,m3;
+//	Scanner sc=new Scanner(System.in);
+//	System.out.println("Enter your physics marks");
+//	m1=sc.nextByte();
+//	System.out.println("Enter your chemistry marks");
+//	m2=sc.nextByte();
+//	System.out.println("Enter your maths marks");
+//	m3=sc.nextByte();
+//	float avg=(m1+m2+m3)/3.0f;
+//	if(avg>=40 && m1>=33 && m2>=33 && m3>=33) {
+//		System.out.println("congratulation you have been promoted");
+//	}
+//	else {
+//		System.out.println("you have not been promoted");
+//	}
+	
+//	question 3
+//	System.out.println("Enter number");
+//	Scanner sc=new Scanner(System.in);
+//	int day=sc.nextInt();
+//	switch(day) {
+//	case 1 :System.out.println("monday");
+//	break;
+//	case 2 :System.out.println("tuesday");
+//	break;
+//	case 3 :System.out.println("wednesday");
+//	break;
+//	case 4 :System.out.println("thursday");
+//	break;
+//	case 5 :System.out.println("friday");
+//	break;
+//	case 6 :System.out.println("saturday");
+//	break;
+//	case 7 :System.out.println("sunday");
+//	
+//	}
+		
+}
+}
