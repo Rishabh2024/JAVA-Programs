6ccec2fabaaee01689e1b992bd2850c7c8ae22c5
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-04 12:09:17 +0530

    Add files via upload
    Looping in programming languages is a feature which facilitates the execution of a set of instructions/functions repeatedly while some ...

---
+++ CWH_21_Loops.java
@@ -0,0 +1,24 @@
+package core;
+
+public class CWH_21_Loops {
+	public static void main(String[] args) {
+//	1 print num 1 to 100
+//		int i = 100;
+//		for (i = 1; i <= 100; i++) {
+//			System.out.println(i);
+//		}
+
+//int i=1;
+//while(i<=3) {
+//	System.out.println(i++);
+//}
+//System.out.println("finish running loop");
+
+//2 print 100 to 200 natural num
+int a=100;
+while(a<201) {
+	System.out.println(a++);
+}
+
+	}
+}
