664395d7d9a0f59d83a73a6ed70446390b404d07
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-05 10:17:46 +0530

    Add files via upload
    The do… while in C is a loop statement used to repeat some part of the code till the given condition is fulfilled.

---
+++ CWH_22_DoWhile.java
@@ -0,0 +1,16 @@
+package core;
+
+public class CWH_22_DoWhile {
+	public static void main(String[] args) {
+//	int a=0;
+//	while(a<5) {
+//		System.out.println(a++);
+//	}
+
+		int b = 0;
+		do {
+			System.out.println(b++);
+
+		} while (b < 5);
+	}
+}
