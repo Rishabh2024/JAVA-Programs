cae38bc1e6aadc5d94584092fef77ba2c4e20867
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-07 13:04:21 +0530

    Add files via upload
    Java for loop helps execute a set of code repeatedly, and it is recommended when the number of iterations is fixed.

---
+++ CWH_23_Forloop.java
@@ -0,0 +1,33 @@
+package core;
+
+public class CWH_23_Forloop {
+public static void main(String[] args) {
+	
+//	for(int i=0; i<=10; i++){
+//		System.out.println(i);
+//	}
+	
+//	find 5 even num
+//	2i= Even num=2,4,8,10 formula
+	int n=5;
+	for(int i=0; i<5; i++) {
+		System.out.println(2*i);
+	}
+	
+//	find 5 odd num
+//	2i+1=odd num =3,5,7,9
+//	int p=5;
+//	for(int i=0; i<5; i++) {
+//		System.out.println(2*i+1);		
+//	}
+	
+//	Decrementing for loop
+	
+//	for(int i=5; i>0; i--) {
+//		System.out.println(i);
+//	}
+	
+	
+	
+}
+}
