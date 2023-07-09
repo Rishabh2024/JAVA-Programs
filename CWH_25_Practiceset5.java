ac4d508bc7b83fbd66e553c2f2d0d86074cf882b
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-09 12:36:04 +0530

    Add files via upload
    In This code I have Done My Practice question of factorial ,Pattern ,Sum.

---
+++ CWH_25_Practiceset5.java
@@ -0,0 +1,44 @@
+package core;
+
+public class CWH_25_Practiceset5 {
+public static void main(String[] args) {
+//	print pattern
+//	int n=4;
+//	for(int i=n; i>0; i--) {
+//		for(int j=0; j<i;j++) {
+//		System.out.print("*");	
+//	}  
+//		System.out.print("\n"); 
+//}
+	
+//	problem 2
+//	int sum=0;
+//	int n=3;
+//	for(int i=0;i<n;i++) {
+//		sum=sum+(2*i);		
+//	}
+//	System.out.println("sum of even number is" + sum);
+   
+//	problem 3
+//	print table
+//	int i=5;  
+//	for(i=1; i<=10;i++) {  
+//		System.out.printf("%d X %d=%d\n",  i*i);
+//	}
+	
+//	problem 4
+	
+
+//	problem 6  factorial 5*4*3*2*1
+//	
+
+//	problem 9
+	int n=8;
+	int sum=0;
+	for(int i=0; i<=10;i++) {
+		sum +=n*i;	
+	}
+	System.out.println(sum);
+	
+}
+}
