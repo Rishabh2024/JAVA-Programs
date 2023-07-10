e4728f6493c8241af0db4d5af81340ce753a3b4b
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-10 12:50:33 +0530

    Add files via upload
    simple array print array index and length of array

---
+++ CWH_26_arrays.java
@@ -0,0 +1,16 @@
+package core;
+
+public class CWH_26_arrays {
+	public static void main(String[] args) {
+	int[]marks=new int[5];
+	marks[0]=100;
+	marks[1]=60;
+	marks[2]=69;
+	marks[3]=70;
+	marks[4]=98; 
+//	marks[5]=86;  throws an error
+//		int[] marks = { 55, 66, 87, 78, 95 };
+		System.out.println(marks[3]);
+//		System.out.println(marks.length);
+	}
+}
