4a322ef23f93aea9d3e5b720e686eac207a53f6b
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-11 13:00:03 +0530

    Add files via upload
    Print an arry using for and foreach loop

---
+++ CWH_27_arrays.java
@@ -0,0 +1,35 @@
+package core;   
+
+public class CWH_27_arrays {
+public static void main(String[] args) {
+//	int []marks= {55,66,85,99,78,35,85,74};
+//	float[]marks= {55.5f,68.2f,65.2f};
+//	String[]student= {"akash","shubham","deepak"};
+//System.out.println(student.length);
+//System.out.println(student[1]);
+//	System.out.println(marks.length);;;;;;
+//	System.out.println(marks[2]);
+	     
+//	int []marks= {55,66,85,99,78,35,85,74};
+//	int i=0;
+//	System.out.println(marks.length);;;;;;
+//displaying the array using for loop	
+	
+//	for(i=0;i<marks.length;i++){
+//	System.out.println(marks[i]);	
+//	}
+	 							 												
+//	displaying an array in reverse order
+//	int []marks= {55,66,85,99,78,35,85,74};
+//	for(int i=marks.length-1;i>=0;i--);{
+//		System.out.println(marks[i]);
+//	}
+	
+//	print using for each loop
+	   
+	int []marks= {55,66,85,74};
+	for(int element:marks) {
+	System.out.println(element);
+	}
+}
+}  
