f0a7903eabcaeecf35e6ced124fb9e20400e94ae
Rishabh pal <114098997+Rishabh2024@users.noreply.github.com>
2023-07-14 14:00:01 +0530

    Add files via upload
    My array practice questions 
    Which helps to improve my problem solving approach

---
+++ CWH_29_Practiceset6.java
@@ -0,0 +1,125 @@
+ package core;
+
+public class CWH_29_Practiceset6 {
+public static void main(String[] args) {
+
+//	problem 1
+//	float []marks= {50.4f,65.5f,85.5f,96.5f,75.5f};
+//	float sum=0;
+//	for(float element:marks) {  //for each loop
+//		sum=sum+element;
+//	}
+//	System.out.println("the value of sum is  " + sum);
+	
+//	problem 2
+//	float []marks= {50.4f,65.5f,85.5f,96.5f,75.5f};
+//float num=85.5f;
+//float num=66.8f;
+//boolean isInArray=false;
+//for(float element:marks) {
+//if(num==element){
+//	isInArray=true;
+//	break;
+//}
+//}
+//if(isInArray) {
+//	System.out.println("The value is present in array");
+//}
+//else {
+//	System.out.println("The value is not present in array");
+//}
+
+//problem 3
+//double []marks= {50,65,85,96,75};
+//double sum=0;
+//for(double Rishabh:marks) {  //for each loop
+//sum=sum+Rishabh;
+//}
+//System.out.println("the value of average marks  is  " + sum/marks.length);
+
+
+
+//problem 4
+//int [] []mat1= {{1,2,3},
+//       	    	{4,5,6}};
+//int [] []mat2= {{2,6,13},
+//                {3,7,1}};
+//int [] [] result={{0,0,0},
+//	    	     {0,0,0}};
+//
+//for(int i=0;i<mat1.length;i++) {  //row no of time
+//	for(int j=0;j<mat1[i].length;j++) {  //column no time
+//		System.out.format("setting value for i=%d and j=%d\n",i ,j);
+//result [i][j]=mat1[i][j]+mat2[i][j];		
+//	}
+//}
+//
+//for(int i=0;i<mat1.length;i++) {  //row no of time
+//	for(int j=0;j<mat1[1].length;j++) {  //column no time
+//		System.out.print(result[i][j] +" ");
+//result [i][j]=mat1[i][j]+mat2[i][j];		
+//	}
+//	System.out.println(" ");
+//}
+
+
+//problem 5
+//	
+//	 int [] arr = {1, 21, 3, 4, 5, 34, 67};
+//     int l = arr.length;
+//     int n = Math.floorDiv(l, 2);
+//     int temp;
+//
+//     for(int i=0; i<n; i++){
+//          //Swap a[i] and a[l-1-i]
+//          //a   b   temp
+//          //|4| |3| ||
+//         temp = arr[i];
+//         arr[i] = arr[l-i-1];
+//         arr[l-i-1] = temp;
+//     }
+//
+//     for(int element: arr){
+//         System.out.print(element + " ");
+//     }
+     
+// Practice Problem 6
+//     int [] arr = {1, -2100, -3, 455, 5, 34, 67};
+//     int max = Integer.MIN_VALUE;
+//     for(int e: arr){
+//         if(e>max){
+//             max = e;
+//         }
+//     }
+//     System.out.println("the value of the maximum element in this array is: "+ max);
+
+     // Practice Problem 6
+     System.out.println(Integer.MIN_VALUE);
+     System.out.println(Integer.MAX_VALUE);
+   
+	
+	
+	 // Practice Problem 8 
+//    boolean isSorted = true;
+//    int [] arr = {1, 12, 3, 4, 5, 34, 67};
+//    for(int i=0;i<arr.length-1;i++){
+//        if(arr[i] > arr[i+1]){
+//            isSorted = false;
+//            break;
+//        }
+//    }
+//    if(isSorted){
+//        System.out.println("The Array is sorted");
+//    }
+//    else{
+//        System.out.println("The Array is not sorted");
+//    }
+
+
+
+
+}
+
+}
+
+
